46325f6a0331a74d0b174f5e4b92898ac56c93fd
yvens <yvensre@gmail.com>
2023-02-03 21:34:22 +0100

    Add basic root placement mechanic

---
+++ GGJ2023Unity/Assets/Scripts/Game/RootsDatabase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class RootsDatabase : ScriptableObject
+    {
+        
+    }
+}
