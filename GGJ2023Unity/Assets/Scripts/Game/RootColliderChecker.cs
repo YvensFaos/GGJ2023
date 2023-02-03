becfa9ca745637c31166537958a2ea2e5b858f2f
yvens <yvensre@gmail.com>
2023-02-03 22:24:30 +0100

    Improve collision check

--- GGJ2023Unity/Assets/Scripts/Game/RootColliderChecker.cs
+++ GGJ2023Unity/Assets/Scripts/Game/RootColliderChecker.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Game
@@ -19,20 +20,42 @@ namespace Game
             }
         }
 
-        void Start()
+        private void Start()
         {
+            StartCoroutine(ColliderCheck());
+        }
+        
+        private IEnumerator ColliderCheck()
+        {
+            //Wait for one turn
+            yield return null;
+            
+            //Test
+            boxCollider.enabled = false;
             var selfCollider = boxCollider.transform;
             var isIntersecting = Physics.CheckBox(
-                selfCollider.position,
+                boxCollider.center + selfCollider.position,
                 boxCollider.size * 0.5f,
                 selfCollider.rotation,
                 checkLayers,
                 QueryTriggerInteraction.UseGlobal
             );
+            boxCollider.enabled = true;
 
             Debug.Log(isIntersecting
-                ? "Box collider is intersecting with another collider."
-                : "Box collider is not intersecting with another collider.");
+                ? $"Box collider is intersecting with another collider. {selfCollider.rotation.eulerAngles}"
+                : "NO.");
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (boxCollider == null) return;
+            var selfTransform = boxCollider.transform;
+            var selfTransformPosition = selfTransform.position;
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(boxCollider.center + selfTransformPosition, boxCollider.size.x * 0.5f);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(boxCollider.center + selfTransformPosition, boxCollider.size.y * 0.5f);
         }
     }
 }
